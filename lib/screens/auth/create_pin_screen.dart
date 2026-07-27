fa9c8bdf9a9d43c138f4818cda57dfbcbb29561c
MahinSaeed <mohammedmahinsaeed@gmail.com>
2026-07-27 15:55:26 +0300

    Step 8 - Functional PIN entry UI

--- lib/screens/auth/create_pin_screen.dart
+++ lib/screens/auth/create_pin_screen.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CreatePinScreen extends StatelessWidget {
+import '../../widgets/number_pad.dart';
+import '../../widgets/pin_indicator.dart';
+
+class CreatePinScreen extends StatefulWidget {
   const CreatePinScreen({super.key});
+
+  @override
+  State<CreatePinScreen> createState() => _CreatePinScreenState();
+}
+
+class _CreatePinScreenState extends State<CreatePinScreen> {
+  final List<String> _pin = [];
+
+  void _addDigit(String digit) {
+    if (_pin.length >= 4) return;
+
+    setState(() {
+      _pin.add(digit);
+    });
+
+    if (_pin.length == 4) {
+      Future.delayed(const Duration(milliseconds: 300), () {
+        if (!mounted) return;
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("PIN Entered: ${_pin.join()}"),
+          ),
+        );
+      });
+    }
+  }
+
+  void _removeDigit() {
+    if (_pin.isEmpty) return;
+
+    setState(() {
+      _pin.removeLast();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +47,37 @@ class CreatePinScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Create PIN"),
       ),
-      body: const Center(
-        child: Text(
-          "Create PIN Screen",
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+
+            const SizedBox(height: 30),
+
+            const Text(
+              "Create a 4-digit PIN",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 40),
+
+            PinIndicator(
+              length: 4,
+              currentLength: _pin.length,
+            ),
+
+            const Spacer(),
+
+            NumberPad(
+              onNumberPressed: _addDigit,
+              onBackspace: _removeDigit,
+            ),
+
+            const SizedBox(height: 30),
+          ],
         ),
       ),
     );
